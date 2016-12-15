fecc8ebf2e6c4423b0f2d9681aab889ea6bb2bf3
Clay Cribbs <lclay.cribbs@gmail.com>
2016-12-15 13:09:19 -0500

    fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,3 +12,16 @@ CarrierWave.configure do |config|
   config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
+
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+
+  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+end
