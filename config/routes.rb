7fa563df496db6a924d4bf0cb62a894a36f1344a
Clay Cribbs <lclay.cribbs@gmail.com>
2016-12-15 12:52:32 -0500

    new product path

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get 'admin' => 'products#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
