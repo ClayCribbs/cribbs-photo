07394fc7685172034c393beaf4a43581099fd36e
Clay Cribbs <lclay.cribbs@gmail.com>
2016-12-15 13:12:52 -0500

    changed back

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,16 +11,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
-
-
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-      
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
